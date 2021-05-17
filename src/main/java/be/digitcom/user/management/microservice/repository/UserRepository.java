6db718aacda7c4a54a261f7ff165bc5ad076bf1a
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2021-05-17 13:13:03 +0200

    User Repository

---
+++ src/main/java/be/digitcom/user/management/microservice/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package be.digitcom.user.management.microservice.repository;
+
+import be.digitcom.user.management.microservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    @Query("SELECT u.name FROM User u WHERE u.id IN (:pIdList)")
+    List<User> findUserNames(@Param("pIdList") List<Long> idList);
+}
