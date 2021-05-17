5331967728bf7b39d8817f1176043917313ced40
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2021-05-17 11:30:14 +0200

    Initial state

---
+++ src/main/java/be/digitcom/user/management/microservice/UserManagementMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package be.digitcom.user.management.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementMicroserviceApplication.class, args);
+	}
+
+}
