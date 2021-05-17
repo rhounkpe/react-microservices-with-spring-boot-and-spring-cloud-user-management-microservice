342d41b9c1fd4a71008fae33f211926c9319b7e6
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2021-05-17 12:40:54 +0200

    User model

---
+++ src/main/java/be/digitcom/user/management/microservice/model/Role.java
@@ -0,0 +1,6 @@
+package be.digitcom.user.management.microservice.model;
+
+public enum Role {
+    USER,
+    ADMIN,
+}
---
+++ src/main/java/be/digitcom/user/management/microservice/model/User.java
@@ -0,0 +1,29 @@
+package be.digitcom.user.management.microservice.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "user")
+@Data
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+
+}
